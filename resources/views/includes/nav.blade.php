ba837592f977e48e7c90a81b7c02e7fbc009051d
WyldHalfling <69316827+WyldHalfling@users.noreply.github.com>
2022-04-04 20:25:30 -0500

    Set up home nav bar for mobile and large screen.

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,20 +1,21 @@
 <?php $categories = \App\Models\Category::with('subCategories')->get() ?>
 
 <header class="navigation">
-    <!-- <div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
-        <button class="menu-icon" type="button" data-toggle="example-menu"></button>
-        <div class="title-bar-title">Menu</div>
-      </div> -->
-      
-    <div class="top-bar" id="main-menu" data-dropdown-menu
+
+<div class="title-bar toggle" data-responsive-toggle="main-menu" data-hide-for="medium">
+    <button class="menu-icon" type="button" data-toggle="main-menu"></button>
+    <a href="/" class="float-right small-logo">STORE</a>
+  </div>
+  
+  <div class="top-bar menu" id="main-menu" data-dropdown-menu 
         data-responsive-menu="drilldown meduim-dropdown" data-click-open="true"
         data-disable-hover="true"  data-close-on-click-inside="false">
         <div class="top-bar-title show-for-medium">
-            <a href="/" class="logo">{{$_ENV['APP_NAME']}}</a>
+            <a href="/" class="logo"></a>
         </div>
 
         <div class="top-bar-left">
-          <ul class="dropdown menu vertical medium-horizontal" data-dropdown-menu>
+            <ul class="dropdown menu vertical medium-horizontal" data-dropdown-menu>
                 <li><a href="#">Products</a></li>
                 @if(count($categories))
                     <li>
@@ -40,6 +41,7 @@
                 @endif
             </ul>
         </div>
+        
         <div class="top-bar-right">
             <ul class="dropdown menu vertical medium-horizontal">
                 <li><a href="#">Username</a></li>
@@ -49,30 +51,6 @@
             </ul>
         </div>
     </div>
-<!--
-    <div class="top-bar" id="main-menu">
-    <div class="menu medium-horizontal expanded medium-text-center"
-    data-responsive-menu="drilldown meduim-dropdown" data-click-open="true"
-    data-disable-hover="true" data-close-on-click-inside="false">
 
-        <div class="top-bar-title show-for-medium">
-            <a href="/" class="logo">{{$_ENV['APP_NAME']}}</a>
-        </div>
-
-        <div class="top-bar-left">
-            <ul class="dropdown menu vertical medium-horizontal">
-                <li>Acme Products</li>
-                <li>Categories</li>
-            </ul>
-        </div>
-        <div class="top-bar-right">
-            <ul class="dropdown menu vertical medium-horizontal">
-                <li>Username</li>
-                <li><a href="#">Sign In</a> </li>
-                <li><a href="#">Register</a> </li>
-                <li><a href="#">Cart</a> </li>
-            </ul>
-        </div>
     </div>
-    </div> -->
-</header>
+    </header>
